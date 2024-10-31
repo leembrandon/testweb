f886925c0091f8a31e7bc294a8cff490d819838b
leembrandon <45471006+leembrandon@users.noreply.github.com>
2024-10-31 05:07:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Typing Effect
+var typed = new Typed('#typed', {
+    strings: ["Hi, We Are Global"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true
+  });
